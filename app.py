17406ad662c807f3f0dba85601ee20758db5b59f
Yuvraj03062004 <yuvrajtiwari230@gmail.com>
2025-08-15 16:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import plotly.express as px
-import json
 import requests
 
 # Data load
 df = pd.read_csv("electric_sales.csv")
 
-# India ka GeoJSON load (public source)
-geojson_url = "https://raw.githubusercontent.com/geohacker/india/master/state/india_telengana.json"
-geojson_data = requests.get(geojson_url).json()
+# Extra spaces remove
+df['State'] = df['State'].str.strip()
 
-# Column check
-df['State'] = df['State'].str.strip()  # extra spaces remove
+# India ka GeoJSON load
+geojson_url = "https://raw.githubusercontent.com/geohacker/india/master/state/india_states.geojson"
+geojson_data = requests.get(geojson_url).json()
 
 # Choropleth Map
 fig_map = px.choropleth(
     df,
     geojson=geojson_data,
-    featureidkey="properties.NAME_1",  # GeoJSON me state ka naam yahan hota hai
+    featureidkey="properties.state",  # GeoJSON me state ka naam ka key
     locations="State",
     color="EV_Sales",
     hover_name="State",
@@ -27,8 +26,7 @@ fig_map = px.choropleth(
 
 fig_map.update_geos(fitbounds="locations", visible=False)
 
-# Agar Streamlit me use karna ho
+# Agar Streamlit me
 # import streamlit as st
 # st.plotly_chart(fig_map)
-
 
