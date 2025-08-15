7c0ef28c6b2a21f976844dd0f7a328167745c853
Yuvraj03062004 <yuvrajtiwari230@gmail.com>
2025-08-15 17:02:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("""
     }
     /* KPI Card Styling */
     [data-testid="stMetric"] {
-        background-color: #FFB300;
+        background-color: #E3F2FD;
         border-radius: 12px;
         padding: 15px;
         text-align: center;
@@ -90,4 +90,5 @@ with col5:
 # --- FOOTER ---
 st.markdown("---")
 st.caption("📊 Electric Vehicle Sales Dashboard | Created by [YUVRAJ TIWARI] | Powered by Streamlit")
+
 
