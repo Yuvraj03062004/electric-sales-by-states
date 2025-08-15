400aae9338a237c8079106346844d9e54bee6b9d
Yuvraj03062004 <yuvrajtiwari230@gmail.com>
2025-08-15 16:16:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,22 +25,3 @@ col1, col2 = st.columns(2)
 col1.metric("Total EV Sales", f"{total_sales:,}")
 col2.metric("Top State", top_state)
 
-# Map
-geojson_url = "https://raw.githubusercontent.com/geohacker/india/master/state/india_telengana.json"
-geojson_data = requests.get(geojson_url).json()
-
-fig_map = px.choropleth(
-    df_filtered,
-    geojson=geojson_data,
-    featureidkey="properties.NAME_1",
-    locations="State",
-    color="EV_Sales",
-    hover_name="State",
-    color_continuous_scale="Viridis",
-    title="State-wise EV Sales"
-)
-fig_map.update_geos(fitbounds="locations", visible=False)
-st.plotly_chart(fig_map, use_container_width=True)
-
-
-
